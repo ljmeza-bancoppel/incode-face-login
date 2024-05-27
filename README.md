54d49be4fe01e45b53d37a23665fc361597e8da6
Levhita <armando.arias@incode.com>
2024-05-27 14:55:20 -0600

    imported

---
+++ README.md
@@ -0,0 +1,73 @@
+# Face Authentication Example
+This sample does face authentication following the next process.
+
+```mermaid
+sequenceDiagram
+    participant w as WebSDK
+    participant b as Backend
+    participant a as API
+
+    note over w: create()
+    note over w: renderLogin()<br>Send identityId for 1:1
+    alt faceMatch==false
+        note over w: User doesn't exists
+    else
+        w -->> b: transactionId<br>token<br>interviewId
+        note over b: myapp.com/api/auth
+        note over b: get adminToken
+        b-->> a: transactionId<br>token<br>interviewId<br>adminToken
+        note over a: /omni/authentication/verify
+        a-->>b: verified
+        b-->>w: verified
+        alt verified==true
+            note over w: Authentication Verified
+        else
+            note over w: Authentication is not valid
+        end
+    end
+```
+
+# Requirements
+Vite requires Node.js version 14.18+, 16+. some templates require a higher Node.js
+version to work, please upgrade if your package manager warns about it.
+
+## Backend Server
+A backend server that will generate the url is needed for this sample,
+luckily for you we already have sample server NodeJS, please reffer to our documentation on subject:
+[Backend Sample Server](https://developer.incode.com/docs/code-samples-for-web-integrations#backend-sample-server)
+
+In order to simplfy development, this repo is configured to reverse
+proxy a local backend server (`http://localhost:3000`) in the `/api`
+url like `https://<your-ip>:5173/api`, if you want to point your
+frontend development to a backend server deployed elsewhere, change
+the VITE_TOKEN_SERVER_URL to the full url of such server.
+
+# Install
+Run `npm install`
+
+# Config
+Copy `.env.example` to `.env.local` and add your local values
+```
+VITE_TOKEN_SERVER_URL=/api
+VITE_API_URL=https://demo-api.incodesmile.com
+VITE_SDK_URL=https://sdk.incode.com/sdk/onBoarding-1.70.0.js
+
+#Enable for 1:N
+#VITE_CLIENT_ID=<your client id>
+#VITE_API_KEY=<your api key>
+```
+
+# Run
+Vite is configured to serve the project using https and and expose him self,
+so you can easily test with your mobile phone on the local network.
+
+run `npm run dev`
+
+A new server will be exposed, the data will be in the terminal
+
+# Build
+run `npm run build`
+
+A new build will be created in `/dist` you can serve that build everywhere
+just remember to serve with https.
+
---
+++ main.js
@@ -0,0 +1,81 @@
+const tokenServerURL= import.meta.env.VITE_TOKEN_SERVER_URL
+let onBoarding;
+
+const mainContainer = document.getElementById("app");
+const loginContainer = document.getElementById("login");
+const loginButton = document.getElementById("login-button");
+
+
+function showError(e=null) {
+  mainContainer.innerHTML = "Something Went Wrong, see console for details...";
+  console.log(e)
+}
+
+function identifyUser(identityId){
+  onBoarding.renderLogin(mainContainer,{
+    onSuccess: async (response) => {
+      const {token, transactionId, interviewToken, faceMatch, customerId, email} = response;
+      if (faceMatch){
+        // User has an Incode Identity.
+        // Verify using your backend that the faceMatch was actually valid and
+        // not man in the middle attack
+        const response = await fetch(`${tokenServerURL}/auth`,
+        {
+          method: "POST",
+          mode: "cors", 
+          body: JSON.stringify({token,transactionId: transactionId, interviewToken})
+        }
+      );
+      const verification = await response.json();
+      if(verification.verified===true){
+        finish(customerId, email);
+      } else {
+        showError(new Error("FaceMatch is invalid."));
+      }
+    } else {
+      showError(new Error("Face did not match any user."));
+    }
+  },
+  onError: error => {
+    showError(error)
+    // User not found. Add rejection your logic here
+  },
+  isOneToOne: true,
+  oneToOneProps: {
+    identityId: identityId,
+  }
+});
+}
+
+function finish(customerId, email) {
+  mainContainer.innerHTML = `Sucessfull Login:<br/>\n<div>CustomerId: ${customerId}</div>\n<div>Email: ${email}</div>`;
+}
+
+async function app() {
+  // Create the instance of incode linked to a client
+  const apiURL = import.meta.env.VITE_API_URL;
+  // Enable for 1:N
+  //const clientId = import.meta.env.VITE_CLIENT_ID;
+  //const apiKey = import.meta.env.VITE_API_KEY;
+  
+  onBoarding = window.OnBoarding.create({
+    apiURL
+    // clientId, // Enable for 1:N
+    // apiKey // Enable for 1:N
+  });
+  
+  // Incode web_sdk need to preload some core component before being usable
+  mainContainer.innerHTML = "Warming up...";
+  await onBoarding.warmup();
+  
+  // Empty the message and starting the flow
+  mainContainer.innerHTML = "";
+  loginContainer.style.display="flex";
+  loginButton.addEventListener('click', () =>{
+    const identityIdInput = document.getElementById("identity-id");
+    loginContainer.style.display="none";
+    identifyUser(identityIdInput.value)
+  })
+}
++
+document.addEventListener("DOMContentLoaded", app);
