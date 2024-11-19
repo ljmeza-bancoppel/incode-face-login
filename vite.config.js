71740b81b29c030f70a55df298ad9efa053b3629
Lenin Meza <e_ljmeza@bancoppel.com>
2024-11-19 12:53:55 -0600

    Configuration for production

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  server: {
-    https: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
+export default defineConfig(({ mode }) => {
+  // Load the current env file (production in build case) located in the environment directory
+  const env = loadEnv(mode, 'environment');
+
+  return {
+    server: {
+      https: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      },
     },
-  },
-  plugins: [mkcert()],
-  base: '/incode-face-login/',
+    plugins: [mkcert()],
+    base: '/incode-face-login/',
+    envDir: 'environment',
+  }
 })
