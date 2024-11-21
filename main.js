8164d41bf6afa721dbad4ac1c7b37e88d03e39c5
Lenin Meza <e_ljmeza@bancoppel.com>
2024-11-21 14:20:52 -0600

    Styles added

--- main.js
+++ main.js
@@ -7,58 +7,79 @@ const loginButton = document.getElementById("login-button");
 
 
 function showError(e = null) {
-  mainContainer.innerHTML = "Something Went Wrong, see console for details...";
+  mainContainer.innerHTML = "Algo ha salido mal, ver consola para más detalles...";
   console.log(e)
 }
 
 function identifyUser(identityId) {
-  onBoarding.renderLogin(mainContainer, {
-    onSuccess: async (response) => {
-      const { token, transactionId, interviewToken, faceMatch, customerId, email } = response;
-      if (faceMatch) {
-        console.log("token:", token);
-        console.log("transactionId:", transactionId);
-        console.log("interviewToken:", interviewToken);
-        console.log("faceMatch:", faceMatch);
-        console.log("customerId:", customerId);
-        console.log("email:", email);
+  // NotFoundError: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.
+  try {
+    onBoarding.renderLogin(mainContainer, {
+      onSuccess: async (response) => {
+        const { token, transactionId, interviewToken, faceMatch, customerId, email } = response;
+        console.log("response:", response);
+        alert(`faceMatch: ${faceMatch}`);
+        if (faceMatch) {
+          console.log("token:", token);
+          console.log("transactionId:", transactionId);
+          console.log("interviewToken:", interviewToken);
+          console.log("faceMatch:", faceMatch);
+          console.log("customerId:", customerId);
+          console.log("email:", email);
 
-        finish(customerId, email, token, transactionId, interviewToken, faceMatch);
+          setTimeout(() => {
+            finish(customerId, email, token, transactionId, interviewToken, faceMatch);
+            console.log("Waited for 5 seconds");
+          }, 5000);
 
-        // User has an Incode Identity.
-        // Verify using your backend that the faceMatch was actually valid and
-        // not man in the middle attack
-        /**
-        const response = await fetch(`${tokenServerURL}/auth`,
-          {
-            method: "POST",
-            mode: "cors",
-            body: JSON.stringify({ token, transactionId: transactionId, interviewToken })
-          }
-        );
-        const verification = await response.json();
-        if (verification.verified === true) {
-          finish(customerId, email);
+          // User has an Incode Identity.
+          // Verify using your backend that the faceMatch was actually valid and
+          // not man in the middle attack
+          /**
+          const response = await fetch(`${tokenServerURL}/auth`,
+            {
+              method: "POST",
+              mode: "cors",
+              body: JSON.stringify({ token, transactionId: transactionId, interviewToken })
+            }
+          );
+          const verification = await response.json();
+          if (verification.verified === true) {
+            finish(customerId, email);
+          } else {
+            showError(new Error("FaceMatch is invalid."));
+          } */
         } else {
-          showError(new Error("FaceMatch is invalid."));
-        } */
-      } else {
-        showError(new Error("Face did not match any user."));
+          setTimeout(() => {
+            showError(new Error("Face did not match any user."));
+            console.log("Waited for 5 seconds");
+          }, 5000);
+        }
+      },
+      onError: error => {
+        showError(error)
+        // User not found. Add rejection your logic here
+      },
+      isOneToOne: true,
+      oneToOneProps: {
+        identityId: identityId,
       }
-    },
-    onError: error => {
-      showError(error)
-      // User not found. Add rejection your logic here
-    },
-    isOneToOne: true,
-    oneToOneProps: {
-      identityId: identityId,
-    }
-  });
+    });
+  } catch (error) {
+    alert('Error!!');
+    console.error(error);
+  }
 }
 
 function finish(customerId, email, token, transactionId, interviewToken, faceMatch) {
-  mainContainer.innerHTML = `Sucessfull Login:<br/>\n<div>CustomerId: ${customerId}</div>\n<br/><div>Email: ${email}</div>\n<br/><div>token: ${token}</div>\n<br/><div>transactionId: ${transactionId}</div>\n<br/><div>interviewToken: ${interviewToken}</div>\n<br/><div>faceMatch: ${faceMatch}</div>`;
+  let message = `<div id="result"><p>Sucessfull Login:</p>\n`;
+  message += `<div><p>CustomerId: ${customerId}</p></div>\n`
+  message += `<div><p>Email: ${email}</p></div>\n`;
+  // message += `<div>token: ${token}</p></div>\n`;
+  message += `<div>transactionId: ${transactionId}</p></div>\n`;
+  // message += `<div>interviewToken: ${interviewToken}</p></div>\n`;
+  message += `<div>faceMatch: ${faceMatch}</div></div>`;
+  mainContainer.innerHTML = message;
 }
 
 async function app() {
@@ -81,11 +102,24 @@ async function app() {
   // Empty the message and starting the flow
   mainContainer.innerHTML = "";
   loginContainer.style.display = "flex";
-  loginButton.addEventListener('click', () => {
+  loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
     const identityIdInput = document.getElementById("identity-id");
-    loginContainer.style.display = "none";
-    identifyUser(identityIdInput.value)
+    if (required(identityIdInput)) {
+      loginContainer.style.display = "none";
+      identifyUser(identityIdInput.value.trim())
+    }
   })
 }
+
+// If the length of the element's string is 0 then display helper message 
+function required(inputtx) {
+  if (inputtx.value.trim().length == 0) {
+    alert("Completa el campo");
+    return false;
+  }
+  return true;
+}
+
 
 document.addEventListener("DOMContentLoaded", app);
